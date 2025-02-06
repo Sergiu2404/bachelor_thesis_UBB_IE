eef5d2902cdc5048fe03161d8ad3e50ee8aed2a9
Goian-Sergiu <sergiu_goian@yahoo.com>
2025-02-06 21:15:09 +0200

    portfolio frontend

---
+++ practical_work/fastapi_auth/lib/presentation/pages/demo_investing/stocks/stock_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StockDetailsPage extends StatefulWidget {
+  const StockDetailsPage({super.key});
+
+  @override
+  State<StockDetailsPage> createState() => _StockDetailsPageState();
+}
+
+class _StockDetailsPageState extends State<StockDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

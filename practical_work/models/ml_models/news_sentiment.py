70e0dc2362c23b783ca511e20459084f15f875fa
Goian-Sergiu <sergiu_goian@yahoo.com>
2025-02-11 12:49:50 +0200

    pretrained news sentiment finbert

---
+++ practical_work/models/ml_models/news_sentiment.py
@@ -0,0 +1,39 @@
+# pretrained finbert model for news sentiment analysis
+
+import feedparser
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="ProsusAI/finbert")
+#print(pipe("Stocks rallied and the British pound gained."))
+
+symbol = "META"
+keyword = "meta" #check for some word in title of the article
+rss_url = f'https://finance.yahoo.com/rss/headline?s={symbol}'
+
+feed = feedparser.parse(rss_url)
+
+total_score = 0
+num_articles = 0
+
+for i, entry in enumerate(feed.entries):
+    if keyword.lower() not in entry.summary.lower():
+        continue
+
+    print(f"title: {entry.title}")
+    print(f"link: {entry.link}")
+    print(f"published: {entry.published}")
+    print(f"summary: {entry.summary}")
+
+    sentiment = pipe(entry.summary)[0]
+    print(f"Sentiment: {sentiment["label"]}, score: {sentiment["score"]}")
+    print("-" * 20)
+
+    if sentiment['label'] == 'positive':
+        total_score += sentiment['score']
+        num_articles += 1
+    elif sentiment['label'] == 'negative':
+        total_score -= sentiment['score']
+        num_articles += 1
+
+final_score = total_score / num_articles
+print(f"Overall sentiment for news regarding {symbol} is {final_score}, so news is mostly {"positive" if final_score >= 0.15 else "negative" if final_score <= -0.15 else "neutral"}")
